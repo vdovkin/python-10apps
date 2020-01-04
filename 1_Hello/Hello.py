1615d50ab3ca46ac060134341a9f20b558b099f2
Vdovkin Oleksandr <a.a.vdovkin@gmail.com>
2020-01-04 13:36:19 +0200

    Change hello

--- 1_Hello/Hello.py
+++ 1_Hello/Hello.py
@@ -1,7 +1,31 @@
-print("********************")
-print(" HELLO APP ")
-print("********************")
+def start():
+    print("********************")
+    print("    HELLO APP   ")
+    print("********************")
 
-Name = input("What is your name? ")
 
-print("Hello " + Name)
+def get_name():
+
+    name = None
+
+    while not name:
+        try:
+            name = input("What is your name? ")
+        except Exception:
+            print("Somethin is wrong")
+
+    return name
+
+
+def grettings(name):
+    print("Hello, {}".format(name))
+
+
+def main():
+    start()
+    name = get_name()
+    grettings(name)
+
+
+if __name__ == "__main__":
+    main()
